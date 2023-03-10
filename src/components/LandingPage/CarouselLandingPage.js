bf238ea0b15b3f6f1a046cfe15fd184e50bfed26
MalinW <maalinwallander@gmail.com>
2023-03-10 21:39:33 +0100

    carousel landing page start

--- src/components/LandingPage/CarouselLandingPage.js
+++ src/components/LandingPage/CarouselLandingPage.js
@@ -1,7 +1,47 @@
-import "./CarouselLandingPage.css";
+import { useRef, useEffect } from "react";
+import { register } from "swiper/element/bundle";
+import CarouselCard from "./CarouselCard";
+import "./Carousel.css";
 
-function CarouselLandingPage() {
-  return <div> Carousel</div>;
-}
+register();
 
-export default CarouselLandingPage;
+export const Carousel = () => {
+  const swiperElRef = useRef(null);
+
+  useEffect(() => {
+    swiperElRef.current.addEventListener("progress", (e) => {
+      const [progress] = e.detail;
+      console.log(progress);
+    });
+
+    swiperElRef.current.addEventListener("slidechange", (e) => {
+      console.log("slide changed");
+    });
+  }, []);
+
+  return (
+    <swiper-container
+      ref={swiperElRef}
+      slides-per-view="1"
+      navigation="true"
+      pagination="true"
+      autoplay="true"
+      delay="500"
+      loop="true"
+      //pauseOnMouseEnter="true"
+    >
+      <swiper-slide>
+        <CarouselCard />
+      </swiper-slide>
+      <swiper-slide>
+        <CarouselCard />
+      </swiper-slide>
+      <swiper-slide>
+        <CarouselCard />
+      </swiper-slide>
+      ...
+    </swiper-container>
+  );
+};
+
+export default Carousel;
