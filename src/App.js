373a7fb956dffe579c1b2037c410872329806f23
MalinW <maalinwallander@gmail.com>
2023-03-13 22:43:13 +0100

    first commit abuot us page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import LandingPage from "./components/LandingPage/LandingPage";
+import AboutUs from "./components/AboutUs/AboutUs";
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      <AboutUs />
     </div>
   );
 }
---
+++ src/components/AboutUs/AboutUs.js
@@ -0,0 +1,12 @@
+import "./AboutUs.css";
+import AboutCompany from "./AboutCompany";
+
+function AboutUs() {
+  return (
+    <div>
+      <AboutCompany />
+    </div>
+  );
+}
+
+export default AboutUs;
