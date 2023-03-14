7b001761a8ab674d3378aa85aceb49aa80d6e8ba
MalinW <maalinwallander@gmail.com>
2023-03-14 10:10:46 +0100

    first page about us

--- src/components/AboutUs/AboutCompany.js
+++ src/components/AboutUs/AboutCompany.js
@@ -1,27 +1,31 @@
 import "./AboutCompany.css";
+import imageAboutUs from "./imageAboutUs.png";
 
 function AboutCompany() {
   return (
     <div className="aboutCompanyContainer">
       <h2 className="aboutCompanyTitle">Om oss</h2>
-      <img src="" alt="" className="aboutCompanyImage" />
-      <div className="aboutCompanyText">
-        <h3>
-          Ett företag med syfte att stödja vårt samhälles pågående
-          paradigmskifte
-        </h3>
-        <p>
-          Vi är en del av Björnbacka retreatcenter, ett företag med syfte att
-          stödja vårt samhälles pågående paradigmskifte med bas i inre
-          hållbarhet och navigation.
-        </p>
-        <p>
-          Världen befinner sig i ett skifte - alltifrån hur vi använder jordens
-          resurser till hur vi driver företag och beter oss mot varandra som
-          medmänniskor. Den holistiska hälsan, det vill säga den hälsa som avser
-          hela vårt välbefinnande; det fysiska, mentala, emotionella och
-          existentiella, har blivit alltmer aktuell och efterfrågad.{" "}
-        </p>
+      <div className="infoCompanyContainer">
+        <img src={imageAboutUs} alt="" className="aboutCompanyImage" />
+        <div className="aboutCompanyText">
+          <h3>
+            Ett företag med syfte att stödja vårt samhälles pågående
+            paradigmskifte
+          </h3>
+          <p>
+            Vi är en del av Björnbacka retreatcenter, ett företag med syfte att
+            stödja vårt samhälles pågående paradigmskifte med bas i inre
+            hållbarhet och navigation.
+          </p>
+          <p>
+            Världen befinner sig i ett skifte - alltifrån hur vi använder
+            jordens resurser till hur vi driver företag och beter oss mot
+            varandra som medmänniskor. Den holistiska hälsan, det vill säga den
+            hälsa som avser hela vårt välbefinnande; det fysiska, mentala,
+            emotionella och existentiella, har blivit alltmer aktuell och
+            efterfrågad.
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,10 +1,12 @@
 import "./AboutUs.css";
 import AboutCompany from "./AboutCompany";
+import Line from "../LandingPage/Line";
 
 function AboutUs() {
   return (
     <div>
       <AboutCompany />
+      <Line />
     </div>
   );
 }
