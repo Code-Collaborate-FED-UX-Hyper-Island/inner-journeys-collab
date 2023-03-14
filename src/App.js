612aad0875bbecfe0234271e773c5c6d2fb7ca33
Marcin Dabek <dabudab@gmail.com>
2023-03-14 13:48:58 +0100

    change C to c in components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import LandingPage from "./Components/LandingPage/LandingPage";
+import LandingPage from "./components/LandingPage/LandingPage";
 import "./App.css";
-import Carousel from "./Components/Carousel/Carousel";
-import ConcertsContainer from "./Components/ConcertsContainer/ConcertsContainer";
-import Events1 from "./Components/Events1/Events1";
-import Events2 from "./Components/Events2/Events2";
+import Carousel from "./components/Carousel/Carousel";
+import ConcertsContainer from "./components/ConcertsContainer/ConcertsContainer";
+import Events1 from "./components/Events1/Events1";
+import Events2 from "./components/Events2/Events2";
 
 function App() {
 	return (
