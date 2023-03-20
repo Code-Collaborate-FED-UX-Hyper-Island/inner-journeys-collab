65cc265382e7b1076302024b36dc160dda0ffab4
MalinW <maalinwallander@gmail.com>
2023-03-20 13:02:41 +0100

    css final

--- src/pages/Concerts/Concerts.js
+++ src/pages/Concerts/Concerts.js
@@ -2,14 +2,15 @@ import { useState } from "react";
 import "./Concerts.css";
 import ConcertContainer from "../../components/ConcertsContainer/ConcertsContainer";
 import Carousel from "../../components/Carousel/Carousel";
-import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer";
+import Line from "../../components/LandingPage/Line";
 
 const Concerts = () => {
   return (
     <div>
       <Carousel />
+      <Line />
       <ConcertContainer />
+      <Line />
     </div>
   );
 };
