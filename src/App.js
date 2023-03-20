a5aa491b806d0cb3f28cb829c34822876872992b
MalinW <maalinwallander@gmail.com>
2023-03-20 20:17:21 +0100

    make runnable

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-
-import React from 'react'
-import VideoCarousel from './components/VideoCarousel/VideoCarousel'
+import React from "react";
+import VideoCarousel from "./components/VideoCarousel/VideoCarousel";
 import "./App.css";
 import LandingPageMain from "./pages/LandingPage/LandingPageMain";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -12,39 +11,39 @@ import Concerts from "./pages/Concerts/Concerts";
 import ContactUsPage from "./pages/ContactUsPage/ContactUsPage";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Root />,
-		errorElement: <ErrorPage />,
-		children: [
-			{
-				path: "/",
-				element: <LandingPageMain />,
-			},
-			{
-				path: "/about",
-				element: <AboutUs />,
-			},
-			{
-				path: "/events",
-				element: <EventsAndRetreats />,
-			},
-			{
-				path: "/concerts",
-				element: <Concerts />,
-			},
-			{
-				path: "/contact",
-				element: <ContactUsPage />,
-			},
-			{
-				path: "/home",
-				element: <LandingPageMain />,
-			},
-		],
-	},
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPageMain />,
+      },
+      {
+        path: "/about",
+        element: <AboutUs />,
+      },
+      {
+        path: "/events",
+        element: <EventsAndRetreats />,
+      },
+      {
+        path: "/concerts",
+        element: <Concerts />,
+      },
+      {
+        path: "/contact",
+        element: <ContactUsPage />,
+      },
+      {
+        path: "/home",
+        element: <LandingPageMain />,
+      },
+    ],
+  },
 ]);
 
-function App() {
-	return <RouterProvider router={router} />;
+export default function App() {
+  return <RouterProvider router={router} />;
 }
